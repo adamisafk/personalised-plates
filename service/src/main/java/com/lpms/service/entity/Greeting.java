fb7799a97266e123bf5ad1ca52c7f30c8bd31bd3
Adam Howells <adam.howells@dvla.gov.uk>
2021-03-25 21:28:50 +0000

    Initalised repo, create Spring REST service, add greeting entity and controller, dockerized

---
+++ service/src/main/java/com/lpms/service/entity/Greeting.java
@@ -0,0 +1,18 @@
+package com.lpms.service.entity;
+
+public class Greeting {
+    // Attributes
+    private final long id;
+    private final String content;
+
+    // Constructors
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    // Getters and Setters
+
+    public long getId() { return id; }
+    public String getContent() { return content; }
+}
